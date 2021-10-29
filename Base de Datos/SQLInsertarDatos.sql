e836703500c19af9d143d9fadcbf44f6fbca4eb0
NoeTulian <91611593+NoeTulian@users.noreply.github.com>
2021-10-29 16:50:46 -0300

    se crear FrmConsultarFactura, se insertan articulos en la BD

--- Base de Datos/SQLInsertarDatos.sql
+++ Base de Datos/SQLInsertarDatos.sql
@@ -10,6 +10,15 @@ values ('mesa', 3500)
 insert into articulos (nombre, pre_unitario)
 values ('alacena', 7000)
 
+delete articulos
+
+INSERT INTO articulos(nombre, pre_unitario) values('Mesa',3500)
+INSERT INTO articulos(nombre, pre_unitario) values('Placard',7800)
+INSERT INTO articulos(nombre, pre_unitario) values('Silla',1500)
+INSERT INTO articulos(nombre, pre_unitario) values('Escritorio',6500)
+INSERT INTO articulos(nombre, pre_unitario) values('Biblioteca',8000)
+INSERT INTO articulos(nombre, pre_unitario) values('Vajillero',9900)
+
 select * from facturas
 select* from detalles_factura
 select*from articulos
---
+++ VS/TrabajoPracticoProg/FacturasFront/FrmConsultarFactura.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace FacturasFront
+{
+    partial class FrmConsultarFactura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbFiltros = new System.Windows.Forms.GroupBox();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbResultados = new System.Windows.Forms.GroupBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.chkBaja = new System.Windows.Forms.CheckBox();
+            this.txtEliminar = new System.Windows.Forms.Button();
+            this.btnEliminarFiltro = new System.Windows.Forms.Button();
+            this.gbFiltros.SuspendLayout();
+            this.gbResultados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbFiltros
+            // 
+            this.gbFiltros.Controls.Add(this.btnEliminarFiltro);
+            this.gbFiltros.Controls.Add(this.chkBaja);
+            this.gbFiltros.Controls.Add(this.btnConsultar);
+            this.gbFiltros.Controls.Add(this.lblCliente);
+            this.gbFiltros.Controls.Add(this.textBox1);
+            this.gbFiltros.Controls.Add(this.dateTimePicker2);
+            this.gbFiltros.Controls.Add(this.label2);
+            this.gbFiltros.Controls.Add(this.dateTimePicker1);
+            this.gbFiltros.Controls.Add(this.label1);
+            this.gbFiltros.Location = new System.Drawing.Point(43, 30);
+            this.gbFiltros.Name = "gbFiltros";
+            this.gbFiltros.Size = new System.Drawing.Size(781, 133);
+            this.gbFiltros.TabIndex = 0;
+            this.gbFiltros.TabStop = false;
+            this.gbFiltros.Text = "Criterios de búsqueda";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(32, 30);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(47, 15);
+            this.lblCliente.TabIndex = 5;
+            this.lblCliente.Text = "Cliente:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(564, 57);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(482, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fecha hasta:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(564, 24);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha desde:";
+            // 
+            // gbResultados
+            // 
+            this.gbResultados.Controls.Add(this.dataGridView1);
+            this.gbResultados.Location = new System.Drawing.Point(43, 183);
+            this.gbResultados.Name = "gbResultados";
+            this.gbResultados.Size = new System.Drawing.Size(781, 228);
+            this.gbResultados.TabIndex = 6;
+            this.gbResultados.TabStop = false;
+            this.gbResultados.Text = "Resultados";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(43, 430);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 1;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(754, 176);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(143, 431);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(746, 431);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(118, 90);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(142, 23);
+            this.btnConsultar.TabIndex = 9;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // chkBaja
+            // 
+            this.chkBaja.AutoSize = true;
+            this.chkBaja.Location = new System.Drawing.Point(118, 65);
+            this.chkBaja.Name = "chkBaja";
+            this.chkBaja.Size = new System.Drawing.Size(89, 19);
+            this.chkBaja.TabIndex = 10;
+            this.chkBaja.Text = "Incluir bajas";
+            this.chkBaja.UseVisualStyleBackColor = true;
+            // 
+            // txtEliminar
+            // 
+            this.txtEliminar.Location = new System.Drawing.Point(241, 431);
+            this.txtEliminar.Name = "txtEliminar";
+            this.txtEliminar.Size = new System.Drawing.Size(75, 23);
+            this.txtEliminar.TabIndex = 9;
+            this.txtEliminar.Text = "Eliminar";
+            this.txtEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarFiltro
+            // 
+            this.btnEliminarFiltro.Location = new System.Drawing.Point(305, 90);
+            this.btnEliminarFiltro.Name = "btnEliminarFiltro";
+            this.btnEliminarFiltro.Size = new System.Drawing.Size(142, 23);
+            this.btnEliminarFiltro.TabIndex = 11;
+            this.btnEliminarFiltro.Text = "Eliminar Filtro";
+            this.btnEliminarFiltro.UseVisualStyleBackColor = true;
+            // 
+            // FrmConsultarFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 465);
+            this.Controls.Add(this.txtEliminar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.gbResultados);
+            this.Controls.Add(this.gbFiltros);
+            this.Name = "FrmConsultarFactura";
+            this.Text = "Consultar Factura";
+            this.gbFiltros.ResumeLayout(false);
+            this.gbFiltros.PerformLayout();
+            this.gbResultados.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbFiltros;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbResultados;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEliminarFiltro;
+        private System.Windows.Forms.CheckBox chkBaja;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button txtEliminar;
+    }
+}
